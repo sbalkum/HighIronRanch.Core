82b4144b9dc3692fde7cec862b1ba5d542419f28
Stephen Balkum <stephen@balkum.com>
2015-07-01 10:54:19 -0500

    Added ILogger, v1.7.2

--- src/HighIronRanch.Core/Properties/AssemblyInfo.cs
+++ src/HighIronRanch.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HighIronRanch.Core")]
-[assembly: AssemblyDescription("Core defines IReadModel, IReadModelRepository, and IWriteableReadModelRepository.")]
+[assembly: AssemblyDescription("Core defines service interfaces used across multiple projects.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stephen Balkum")]
 [assembly: AssemblyProduct("HighIronRanch.Core")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.7.2")]
+[assembly: AssemblyFileVersion("1.0.7.2")]
---
+++ src/HighIronRanch.Core/Services/ILogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace HighIronRanch.Core.Services
+{
+	public interface ILogger
+	{
+		void Debug(string context, string message);
+		void Debug(string context, string fmt, params object[] vars);
+		void Debug(string context, Exception exception, string fmt, params object[] vars);
+
+		void Information(string context, string message);
+		void Information(string context, string fmt, params object[] vars);
+		void Information(string context, Exception exception, string fmt, params object[] vars);
+
+		void Warning(string context, string message);
+		void Warning(string context, string fmt, params object[] vars);
+		void Warning(string context, Exception exception, string fmt, params object[] vars);
+
+		void Error(string context, string message);
+		void Error(string context, string fmt, params object[] vars);
+		void Error(string context, Exception exception, string fmt, params object[] vars);
+
+		void TraceApi(string context, string componentName, string method, TimeSpan timespan);
+		void TraceApi(string context, string componentName, string method, TimeSpan timespan, string properties);
+		void TraceApi(string context, string componentName, string method, TimeSpan timespan, string fmt, params object[] vars);
+		void TraceApi(string context, MethodBase methodBase, TimeSpan timespan, string message);
+		void TraceApi(string context, MethodBase methodBase, TimeSpan timespan, string fmt, params object[] vars);
+	}
+}

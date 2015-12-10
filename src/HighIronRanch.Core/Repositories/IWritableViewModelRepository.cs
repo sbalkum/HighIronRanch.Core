1f6e23d17cc97a6ac60a039b143c93a53331f20b
Stephen Balkum <stephen@balkum.com>
2015-12-10 09:45:57 -0600

    Added ICollectionNamer. V1.0.8.0

--- src/HighIronRanch.Core/Repositories/IWritableViewModelRepository.cs
+++ src/HighIronRanch.Core/Repositories/IWritableViewModelRepository.cs
@@ -5,13 +5,13 @@ namespace HighIronRanch.Core.Repositories
 {
     public interface IWritableViewModelRepository : IViewModelRepository
     {
-        Task InsertAsync<T>(T items) where T : IViewModel;
-        void Insert<T>(T items) where T : IViewModel;
+        Task InsertAsync<T>(T item) where T : IViewModel;
+        void Insert<T>(T item) where T : IViewModel;
         Task InsertAsync<T>(IEnumerable<T> items) where T : IViewModel;
         void Insert<T>(IEnumerable<T> items) where T : IViewModel;
 
-        Task SaveAsync<T>(T item) where T : IViewModel;
-        void Save<T>(T item) where T : IViewModel;
+        Task UpdateAsync<T>(T item) where T : IViewModel;
+        void Update<T>(T item) where T : IViewModel;
 
         Task DeleteAsync<T>(T item) where T : IViewModel;
         void Delete<T>(T item) where T : IViewModel;
---
+++ src/HighIronRanch.Core/Services/ICollectionNamer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HighIronRanch.Core.Services
+{
+    public interface ICollectionNamer
+    {
+        string GetCollectionName(Type type);
+    }
+}

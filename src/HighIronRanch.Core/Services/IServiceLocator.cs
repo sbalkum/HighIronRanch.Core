cfcb5b0a94639efee66a2d50e356d7a0a3d61125
sbalkum <stephen@balkum.com>
2017-08-03 14:04:37 -0500

    Added IServiceLocator. V2.0.1

---
+++ src/HighIronRanch.Core/Services/IServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HighIronRanch.Core.Services
+{
+    public interface IServiceLocator
+    {
+        T GetInstance<T>();
+        object GetInstance(Type modelType);
+        T TryGetInstance<T>();
+        object TryGetInstance(Type modelType);
+        IEnumerable<T> GetAllInstances<T>();
+        IEnumerable<object> GetAllInstances(Type modelType);
+        void BuildUp(object target);
+    }
+}
